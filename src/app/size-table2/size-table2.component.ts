7917fbc360e435a9b6368be1af0c2d1db6caac74
Joanna Borkala-Kilian <joannaborkala@gmail.com>
2025-02-23 21:13:41 +0100

    prettier

--- src/app/size-table2/size-table2.component.ts
+++ src/app/size-table2/size-table2.component.ts
@@ -10,7 +10,7 @@ import { CdkTableModule } from '@angular/cdk/table';
   standalone: true,
   templateUrl: './size-table2.component.html',
   styleUrl: './size-table2.component.css',
-  imports: [CdkTableModule , CommonModule ],
+  imports: [CdkTableModule, CommonModule],
 })
 export class SizeTable2Component {
   products = signal<ProductVM[]>([]);
@@ -18,27 +18,29 @@ export class SizeTable2Component {
   readonly vmChildren = toSignal(of(products));
 
   readonly colorSizeMap = new Map<string, Map<string, ProductVM>>();
-  readonly rows = toSignal(of(["Red", "Blue", "Green"]) ?? []);
-  readonly columns = toSignal(of(["M", "L", "S", "XL"]) ?? []);
+  readonly rows = toSignal(of(['Red', 'Blue', 'Green']) ?? []);
+  readonly columns = toSignal(of(['M', 'L', 'S', 'XL']) ?? []);
 
   readonly displayedColumns = computed(() => {
     this.colorSizeMap.clear();
-    this.vmChildren()?.forEach(vm => {
+    this.vmChildren()?.forEach((vm) => {
       if (!vm.product.color) {
         return;
       }
       if (!this.colorSizeMap.has(vm.product.color)) {
         this.colorSizeMap.set(vm.product.color, new Map<string, ProductVM>());
       }
-      const colorMap = this.colorSizeMap.get(vm.product.color) ?? new Map<string, ProductVM>();
+      const colorMap =
+        this.colorSizeMap.get(vm.product.color) ?? new Map<string, ProductVM>();
       colorMap.set(vm.product.size, vm);
     });
 
-    return ['color', ...this.columns() ?? []];
+    return ['color', ...(this.columns() ?? [])];
   });
 
   getProductAmount(color: string, size: string): ProductVM | undefined {
-    const colorMap = this.colorSizeMap.get(color) ?? new Map<string, ProductVM>();
+    const colorMap =
+      this.colorSizeMap.get(color) ?? new Map<string, ProductVM>();
     return colorMap.get(size);
   }
 }
